3e7137c91439cf1e8c1d00cb340e8bbc3c238480
Kim Laï Trinh <kimlai@lrqdo.fr>
2016-10-06 00:09:16 +0200

    Add Select for the date range

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
 import React, { Component } from 'react';
 import Geosuggest from 'react-geosuggest';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: null,
+            when: null,
+            page: 'search',
+        };
+    }
+
     onLocationSelected(suggestion) {
-        console.log(suggestion);
         this.setState({
-            location: suggestion.location
+            location: {
+                coordinates: suggestion.location,
+                label: suggestion.label
+            }
         });
     }
 
+    onWhenSelected(value) {
+        this.setState({ when: value });
+    }
+
+    onSearch() {
+        this.setState({ page: 'searchResult' });
+    }
+
     render() {
-        return (
-            <div className='search-form'>
-                <Geosuggest
-                    placeholder='Où ?'
-                    country='fr'
-                    onSuggestSelect={this.onLocationSelected.bind(this)}
-                />
-                <input placeholder='Quand ?'/>
-                <button>Rechercher</button>
-            </div>
-       );
+        const whenOptions = [
+            { value: 'thisWeek', label: 'Cette semaine' },
+            { value: 'thisMonth', label: 'Ce mois-ci' },
+            { value: 'nextSixMonth', label: 'Dans les 6 prochains mois' },
+        ];
+        switch (this.state.page) {
+            case 'search':
+                return (
+                    <div className='search-form'>
+                        <Geosuggest
+                            placeholder='Où ?'
+                            country='fr'
+                            onSuggestSelect={this.onLocationSelected.bind(this)}
+                        />
+                        <div className='search-when'>
+                            <Select
+                                placeholder='Quand ?'
+                                name='search-when'
+                                options={whenOptions}
+                                value={this.state.when}
+                                searchable={false}
+                                clearable={false}
+                                onChange={this.onWhenSelected.bind(this)}
+                            />
+                        </div>
+                        <button onClick={this.onSearch.bind(this)}>
+                            Rechercher
+                        </button>
+                    </div>
+               );
+            case 'searchResult':
+                return (
+                    <div className='search-result'>
+                        <div>{this.state.location.label}</div>
+                        <div>{this.state.when.label}</div>
+                    </div>
+                );
+            default:
+                return null;
+        }
     }
 }
 
