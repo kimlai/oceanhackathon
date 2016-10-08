93db91d40b4fc8eed03112012d50555d1f441cb1
Chojnacki <alexandre.04@hotmail.fr>
2016-10-08 21:11:09 +0200

    Ajout apm / nettoyage

--- src/PlanningEntry.js
+++ src/PlanningEntry.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PlanningEntryInfo from './PlanningEntryInfo';
 
-import data from './test.json'
-
 class Planning extends Component {
     constructor(props) {
         super(props);
@@ -26,30 +24,28 @@ class Planning extends Component {
         mapContainer.id = 'planning-entry-map-container';
         document.body.appendChild(mapContainer);
         const map = window.L.map('planning-entry-map-container', { scrollWheelZoom: true }).setView(position, 15);
-        window.L.tileLayer(
+        var mapbox = window.L.tileLayer(
             'https://api.mapbox.com/styles/v1/kimlai/city4g5cd00b22iqiwvyfdlv4/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2ltbGFpIiwiYSI6ImNpdHg4b3psMDAwMnAzd29hZ2VrbzVmeTcifQ.JEzjYNojtEPRBove3beibA',
             { maxZoom: 18 })
         .addTo(map);
-        var polygon = window.L.polygon([
-        [51.509, -0.08],
-        [51.503, -0.06],
-        [51.51, -0.047]
-        ]).addTo(map);
 
-        function onEachFeature(feature, layer) {
-        // does this feature have a property named popupContent?
-        if (feature.properties && feature.properties.popupContent) {
-            layer.bindPopup(feature.properties.OBJECTID);
-        }
-        }
-       // $.getJSON("test.geojson",function(data){
-        // add GeoJSON layer to the map once the file is loaded
-        // polygon = window.L.geoJson(data).addTo(map);
-        window.L.geoJSON(data, {
-            onEachFeature: onEachFeature
-        }).addTo(map);
-        //});
+        var wmsLayer = window.L.tileLayer.wms('http://www.ifremer.fr/services/wms1', {
+                layers: 'Aires marines protégées',
+                format: 'image/png',
+                transparent: true,
+            }).addTo(map);
+
+        var baseMaps = {
+            "Mapbox" : mapbox
+        };
+
+        var overlayMaps = {
+            "Aires marines protégées" : wmsLayer
+        };
+
         this.map = map;
+        wmsLayer.remove();
+        window.L.control.layers(baseMaps, overlayMaps).addTo(map);
     }
 
     render() {
