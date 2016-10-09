590f5d5ee746a1e38615e8821773e07563998335
Chojnacki <alexandre.04@hotmail.fr>
2016-10-09 02:40:51 +0200

    Petites modifs + ébauche page 3
    Il y a aussi une image pour la légende des AMP, mais le lien est cassé.

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
 
 class Feedback extends Component {
+
+
+
     render() {
         return (
+		<div>
         	<div>
-        		<h1>Feedback</h1>
-            	<div>Feedback</div>
+        		<h1>Carnet de pêche</h1>
+            	<div> Ma dernière sortie </div>
+            	<li> Aucune information sur la dernière sortie. </li><br/>
+            	<button id = "renseigner"> renseigner </button>
+             </div>
+             <br/><br/>
+             <div>
+        		<h1>Mes anciennes sorties</h1>
+            	<div> Sortie du: ... au ... </div>
+            	<li> Observations: ... </li>
+            	<li> Butin / carnet de pêche: ... </li><br/>
+            	<button id = "see_more"> Voir + </button>
+
             </div>
+            <br/>
+    		<button id = "home"> Planifier une nouvelle sortie </button>
+
+
+             <br/><br/>
+             <br/><br/>
+
+        	<div>
+        		<h1> + D'infos sur mes zones de pêche </h1>
+        		<button> Gestion </button> <span class = "spacer"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><button> Enjeux patrimoine nat. </button> <br/><br/>
+				<button> Enjeux patrimoine culturel </button><span class = "spacer">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> <button> La pêche au siècle dernier </button>
+
+            </div>
+            <br/>
+		</div>
         );
     }
+
 }
 
 export default Feedback;
