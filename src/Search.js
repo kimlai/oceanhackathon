6c68549005e53571499c05ef9b913c585fc4c0fe
kimlai <kimlai.t@gmail.com>
2016-10-09 11:57:37 +0200

    Add other activities in search form

--- src/Search.js
+++ src/Search.js
@@ -53,32 +53,50 @@ class Search extends Component {
             { value: 'thisMonth', label: 'Ce mois-ci' },
             { value: 'nextSixMonth', label: 'Dans les 6 prochains mois' },
         ];
+        const whatOptions = [
+            { value: 'fishing', label: 'Pêche à pied' },
+            { value: 'fake', label: 'Randonnée' },
+            { value: 'fake', label: 'Sports nautiques' },
+            { value: 'fake', label: 'Naturisme' },
+            { value: 'fake', label: 'Pokemon Go' },
+        ];
         return (
             <div>
                 <Navigation />
-                <div className='search-form'>
-                    <Geosuggest
-                        placeholder='Où ?'
-                        country='fr'
-                        onSuggestSelect={this.onLocationSelected.bind(this)}
-                    />
-                    <div className='search-when'>
-                        <Select
-                            placeholder='Quand ?'
-                            name='search-when'
-                            options={whenOptions}
-                            value={this.state.when}
-                            searchable={false}
-                            clearable={false}
-                            onChange={this.onWhenSelected.bind(this)}
+                <div className='search-from-container'>
+                    <div className='search-form'>
+                        <Geosuggest
+                            placeholder='Où ?'
+                            country='fr'
+                            onSuggestSelect={this.onLocationSelected.bind(this)}
                         />
+                        <div className='search-when'>
+                            <Select
+                                placeholder='Quand ?'
+                                name='search-when'
+                                options={whenOptions}
+                                value={this.state.when}
+                                searchable={false}
+                                clearable={false}
+                                onChange={this.onWhenSelected.bind(this)}
+                            />
+                        </div>
+                        <div className='search-what'>
+                            <Select
+                                name='search-when'
+                                options={whatOptions}
+                                value='fishing'
+                                searchable={false}
+                                clearable={false}
+                            />
+                        </div>
+                        <button
+                            onClick={this.onSubmit.bind(this)}
+                            disabled={!(this.state.when && this.state.where)}
+                        >
+                            Rechercher
+                        </button>
                     </div>
-                    <button
-                        onClick={this.onSubmit.bind(this)}
-                        disabled={!(this.state.when && this.state.where)}
-                    >
-                        Rechercher
-                    </button>
                 </div>
             </div>
        );
